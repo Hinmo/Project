00b600099161eff9f0b5f0549294a688d903db54
Mario Jurado <jorjuroba@gmail.com>
2024-02-13 23:04:47 -0500

    Home-Primero

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,17 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../shared/header/header.component";
+import { MenuComponent } from "../../shared/menu/menu.component";
+import { CarouselComponent } from "../../shared/carousel/carousel.component";
+import { TableComponent } from "../../components/table/table.component";
+import { PersonaComponent } from "../../shared/persona/persona.component";
+import { FooterComponent } from "../../shared/footer/footer.component";
 
 @Component({
-  selector: 'app-home',
-  standalone: true,
-  imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+    selector: 'app-home',
+    standalone: true,
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css',
+    imports: [HeaderComponent, MenuComponent, CarouselComponent, TableComponent, PersonaComponent, FooterComponent]
 })
 export class HomeComponent {
 
